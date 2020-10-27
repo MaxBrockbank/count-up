9538105b28c44e13de8a4ddaaea5ae8175a66f48
agatakolohe <agatakolohe@gmail.com>
2020-10-27 11:05:19 -0700

    create logic tree for if certain conditions are met show red border on inputs, else show number range, create class for red border
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
   $("#count").submit(function (event) {
     event.preventDefault();
+
+    const countTo = parseInt($("#count-to").val());
+    const multiple = parseInt($("#multiple").val());
+
+    if (
+      countTo !== NaN &&
+      multiple !== NaN &&
+      countTo > 0 &&
+      multiple > 0 &&
+      multiple < countTo
+    ) {
+      for (let i = 0; i <= countTo; i += multiple) {
+        $(".result").append("<li>" + i + "</li>");
+        $(".form-control").removeClass("wrong");
+      }
+    } else {
+      $(".form-control").addClass("wrong");
+    }
   });
 });
